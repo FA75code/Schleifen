cb90402146f15e5782b1bc336dc5ebd3db0d7d29
EMQcarlos <malte@emq.io>
2017-08-21 08:30:40 +0200

    While Loop implementiert

--- src/Schleifen1.java
+++ src/Schleifen1.java
@@ -1,7 +1,20 @@
 //Zähle alle Teilnehmer
+//"Teilnehmer 1" ... "Teilnehmer 12"
 public class Schleifen1 {
 
 	public static void main(String[] args) {
+		
+		int tnAnzahl = 12; 	// Limit
+		int count = 1;		// Zähler
+		
+		// LOGIK UND AUSGABE
+		// Solange der Zähler kleiner als tnAnzahl ist, soll "Teilnehmer " und Zähler ausgegeben werden
+		while(count <= tnAnzahl)
+		 {
+			System.out.println("Teilnehmer " + count);
+			count++; // count = count + 1;
+		}
+		
 		
 	}
 
