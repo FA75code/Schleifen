b51316ac68c49f319f3e46dd40d80af2d0c37fd9
EMQcarlos <malte@emq.io>
2017-08-21 11:48:30 +0200

    menü aufgabe;

---
+++ src/menue.java
@@ -0,0 +1,16 @@
+
+//Übung: Mehrfachauswahl
+//1. Neue Klasse "Menü" in Projekt Wiederholung 2 erstellen
+//2. Benutzereingabe erstellen: 
+//(1) Computer anschalten 
+//(2) Betriebssystemauswahl 
+//(3) Systemwiederherstellung
+//(0) Computer herunterfahren (ENDE)
+//3. Benutzereingabe auswerten (switch-case)
+//z.B. Bei Eingabe == 2: Ausgabe "Bitte wählen Sie das Betriebssystem aus"
+//4. Eingabe soll solange wiederholt werden, bis Benutzer Computer herunterfährt
+//ZUSATZ: 5. Erstellen Sie ein Untermenü für die Betiebssystemauswahl
+//			 Mit einer Liste von 3 Server- oder Clientbetriebssystemen
+//			 Die Eingabe soll wiederholt werden, 
+//		     bis eine 0 (Zurück zum Hauptmenü) eingegeben wird
+
