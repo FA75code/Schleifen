b07dbbb7a18ccd8f1022c0d989d98d39412e5981
EMQcarlos <malte@emq.io>
2017-08-21 11:49:09 +0200

    Aufgabe 1

--- src/Schleifen4.java
+++ src/Schleifen4.java
@@ -1,15 +1,25 @@
-//1. Zähle alle Teilnehmer von 1 bis 14
-//2. Zähle jeden 2. Teilnehmer
-//3. Zähle alle Teilnehmer von 1 bis 14
+//1. Zï¿½hle alle Teilnehmer von 1 bis 12
+//2. Zï¿½hle jeden 2. Teilnehmer
+//3. Zï¿½hle alle Teilnehmer von 12 bis 1
 //4. Addiere die Teilnemernummern
 //5. Inkrement als Variable
 //6. Endanweisung weglassen
-//7. Zähler in Startanweisung deklarieren
-//8. Teilnehmer 14 mal zählen (verschachtelte for-Schleife)
+//7. Zï¿½hler in Startanweisung deklarieren
+//8. Teilnehmer 12 mal zï¿½hlen (verschachtelte for-Schleife)
 
 public class Schleifen4 {
 
 	public static void main(String[] args) {
+
+		int tnAnzahl = 12;
+		int count = 1;
+		// for(startanweisung; abbruchbedingung; ednanweisung)
+		// for(index; bedingung; index erhÃ¶hen)
+		for (count = 1; count <= tnAnzahl; count++) {
+
+			System.out.println("Teilnehmer " + count);
+			
+		}
 		
 
 	}
