b24bcbb63552275fe23a9ec0790202f4b6ad620f
EMQcarlos <malte@emq.io>
2017-08-21 08:54:56 +0200

    Schleifen3 Typo fix

--- src/Schleifen3.java
+++ src/Schleifen3.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-//1. Menüeingabe mit Mehrfachauswahl
-//2. Menüeingabe wiederholen
-//Datei kann gespeichert und geöffnet sowie das PRogramm beendet werden
-//Alle anderen Eingaben sind ungültig
+//1. MenÃ¼eingabe mit Mehrfachauswahl
+//2. MenÃ¼eingabe wiederholen
+//Datei kann gespeichert und geÃ¶ffnet sowie das PRogramm beendet werden
+//Alle anderen Eingaben sind ungÃ¼ltig
 //Die eingabe wirt solange wiederholt wie das PRogramm nicht beendet wird (Eingabe=0)  
 public class Schleifen3 {
 
