89ba0588eb9fa0ea7a6106c12bf467d73ea0cfe3
EMQcarlos <malte@emq.io>
2017-08-21 13:06:38 +0200

    � in ä geändert

--- src/Schleifen4.java
+++ src/Schleifen4.java
@@ -1,11 +1,11 @@
-//1. Z�hle alle Teilnehmer von 1 bis 12
-//2. Z�hle jeden 2. Teilnehmer
-//3. Z�hle alle Teilnehmer von 12 bis 1
+//1. Zähle alle Teilnehmer von 1 bis 12
+//2. Zähle jeden 2. Teilnehmer
+//3. Zähle alle Teilnehmer von 12 bis 1
 //4. Addiere die Teilnemernummern
 //5. Inkrement als Variable
 //6. Endanweisung weglassen
-//7. Z�hler in Startanweisung deklarieren
-//8. Teilnehmer 12 mal z�hlen (verschachtelte for-Schleife)
+//7. Zähler in Startanweisung deklarieren
+//8. Teilnehmer 12 mal zählen (verschachtelte for-Schleife)
 
 public class Schleifen4 {
 
